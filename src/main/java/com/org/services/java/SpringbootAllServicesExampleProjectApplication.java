e4b24db77f2b83d7e43a4f28ccc0ca7efa649290
javatechi99 <sreenivasarao.techi@gmail.com>
2025-04-09 12:53:58 +0530

    fst cmmt

---
+++ src/main/java/com/org/services/java/SpringbootAllServicesExampleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.org.services.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAllServicesExampleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAllServicesExampleProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/org/services/java/SpringbootAllServicesExampleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.services.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAllServicesExampleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
